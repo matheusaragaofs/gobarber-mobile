82abcf6bdef0f5e6e85305b84f6514467cd8fa65
Matheus <matheusaragaofs@gmail.com>
2021-01-23 15:40:10 -0300

    configurando navegação

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { View, StatusBar, Text } from 'react-native';
+import Routes from './routes';
+import { NavigationContainer } from '@react-navigation/native';
+
+const App: React.FC = () => (
+    <NavigationContainer>
+        <StatusBar barStyle="light-content" backgroundColor="#312e28" />
+        <View style={{ flex: 1, backgroundColor: '#312e28' }}>
+            <Routes />
+            <Text style={{ color: '#fff' }}>oi TUDO BEM</Text>
+        </View>
+    </NavigationContainer>
+);
+export default App;
